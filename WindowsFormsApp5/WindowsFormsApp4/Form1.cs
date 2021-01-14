ad4221d28530bb086e96607bdec3ecee5c0b6249
9A713035 <73284007+9A713035@users.noreply.github.com>
2021-01-14 20:08:53 +0800

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你喜歡豬");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你喜歡鳥");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你喜歡狗");
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你喜歡貓");
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
